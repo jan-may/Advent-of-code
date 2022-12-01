00e78167319f63331b66f86d922e5e0d08657cff
Jan May <jan_may@gmx.net>
2022-12-01 18:41:26 +0100

    day1

---
+++ 01/main.ts
@@ -0,0 +1,25 @@
+// read a txt file
+import { readFileSync } from "fs";
+const text = readFileSync("input.txt", "utf-8");
+
+// split text into array of calories
+const temp = text.split("\n").map(Number);
+
+let max = 0;
+let maxTemp = 0;
+
+let calories: number[][] = [];
+let tempCaloriesArr: number[] = [];
+
+for (let i = 0; i < temp.length; i++) {
+  if (temp[i] !== 0) {
+    tempCaloriesArr.push(temp[i]);
+  } else {
+    calories.push(tempCaloriesArr);
+    tempCaloriesArr = [];
+  }
+}
+
+const totalCalories = calories.map((calorie) => calorie.reduce((a, b) => a + b, 0)).sort((a, b) => a - b);
+
+console.log(totalCalories.at(-1)! + totalCalories.at(-2)! + totalCalories.at(-3)!);
