1fd2fd25045747ac067c859f3b25369291f8f293
Jan May <jan_may@gmx.net>
2023-10-29 13:01:24 +0100

    init rust

---
+++ y_2015/src/main.rs
@@ -0,0 +1,8 @@
+mod day_01;
+mod day_02;
+mod lib;
+
+fn main() {
+    println!("day 1: p1 {} | p2 {}", day_01::part_1(), day_01::part_2());
+    println!("day 2: p1 {} | p2 {}", day_02::part_1(), day_02::part_2());
+}
