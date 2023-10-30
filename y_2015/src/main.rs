c0c673a39ea0b5d455cc628244063952656ea0c2
Jan May <jan_may@gmx.net>
2023-10-30 11:43:40 +0100

    add solution day 5

--- y_2015/src/main.rs
+++ y_2015/src/main.rs
@@ -4,11 +4,14 @@ mod day_01;
 mod day_02;
 mod day_03;
 mod day_04;
+mod day_05;
+
 mod lib;
 
 fn main() {
     // println!("day 1: p1 {} | p2 {}", day_01::part_1(), day_01::part_2());
     // println!("day 2: p1 {} | p2 {}", day_02::part_1(), day_02::part_2());
     // println!("day 3: p1 {} | p2 {}", day_03::part_1(), day_03::part_2());
-    println!("day 4: p1 {} | p2 {}", day_04::part_1(), day_04::part_2());
+    // println!("day 4: p1 {} | p2 {}", day_04::part_1(), day_04::part_2());
+    println!("day 5: p1 {} | p2 {}", day_05::part_1(), day_05::part_2());
 }
