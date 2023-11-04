82333eccb0589d0f94b840f4f285a530c104087a
Jan May <jan_may@gmx.net>
2023-11-04 14:29:43 +0100

    add solution day 17

--- y_2015/src/main.rs
+++ y_2015/src/main.rs
@@ -16,6 +16,7 @@ mod day_13;
 mod day_14;
 mod day_15;
 mod day_16;
+mod day_17;
 
 mod lib;
 
@@ -35,6 +36,7 @@ fn main() {
     // println!("day 13: p1 {} | p2 {}", day_13::part_1(), day_13::part_2());
     // println!("day 14: p1 {} | p2 {}", day_14::part_1(), day_14::part_2());
     // println!("day 15: p1 {} | p2 {}", day_15::part_1(), day_15::part_2());
-    println!("day 16: p1 {} | p2 {}", day_16::part_1(), day_16::part_2());
+    // println!("day 16: p1 {} | p2 {}", day_16::part_1(), day_16::part_2());
+    println!("day 17: p1 {} | p2 {}", day_17::part_1(), day_17::part_2());
 }
 
