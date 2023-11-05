4b4e4bb8edf8462babfd0039d88546e9f76dc374
Jan May <jan_may@gmx.net>
2023-11-05 18:25:05 +0100

    add visualization

--- y_2015/src/day_18.rs
+++ y_2015/src/day_18.rs
@@ -1,10 +1,28 @@
 use crate::lib::parse_input;
+use crossterm::{
+    cursor,
+    execute,
+    terminal::{self, ClearType},
+};
+use std::io::{self, Write};
+use std::thread::sleep;
+use std::time::Duration;
 
 pub fn part_1() -> i32 {
+    let delay = Duration::from_millis(50);
+    // Initialize the terminal
+    terminal::enable_raw_mode().expect("");
+    let mut stdout = io::stdout();
+
+
     let mut grid = generate_grid_from_input();
     for _ in 0..100 {
+        clear_terminal(&mut stdout); // Clear the terminal before rendering the updated grid
+        render_grid(&mut stdout, &grid).expect(""); // Render the grid to the terminal
         grid = update_grid(&grid);
+        sleep(delay);
     }
+    terminal::disable_raw_mode().expect("");
     return sum_active_lights(&mut grid)
 }
 
@@ -85,3 +103,22 @@ fn step(grid: &Vec<Vec<char>>) -> Vec<Vec<char>> {
     let new_grid = update_grid(grid);
     new_grid
 }
+
+fn clear_terminal(stdout: &mut io::Stdout) {
+    // Use terminal control sequences to clear the terminal
+    execute!(stdout, terminal::Clear(ClearType::All), cursor::MoveTo(0, 0)).unwrap();
+    stdout.flush().unwrap();
+}
+
+
+fn render_grid(stdout: &mut io::Stdout, grid: &Vec<Vec<char>>) -> Result<(), Box<dyn std::error::Error>> {
+    for row in grid {
+        for &cell in row {
+            print!("{}", cell);
+        }
+        println!(); // Move to the next line for the next row
+    }
+    stdout.flush()?;
+    Ok(())
+}
+
