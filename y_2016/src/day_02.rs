42347d3076d6e28e38db333a7101396a00cc411f
Jan May <jan_may@gmx.net>
2023-11-10 22:49:27 +0100

    day 01

---
+++ y_2016/src/day_02.rs
@@ -0,0 +1,12 @@
+use crate::lib::parse_input;
+
+pub fn part_1() -> i32 {
+    let input = parse_input("inputs/day_01.txt");
+    println!("{:?}", input);
+    0
+}
+
+pub fn part_2() -> i32 {
+
+    0
+}
