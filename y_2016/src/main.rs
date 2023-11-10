3f8d5d4de819bc0ced2768fedf07fd3a90e844fc
Jan May <jan_may@gmx.net>
2023-11-10 22:49:48 +0100

    day 02

--- y_2016/src/main.rs
+++ y_2016/src/main.rs
@@ -1,8 +1,10 @@
-#[allow(special_module_name)]
+#![allow(dead_code, special_module_name)]
+mod day_02;
 mod day_01;
 
 mod lib;
 
 fn main() {
-    println!("day 1: p1 {} | p2 {}", day_01::part_1(), day_01::part_2());
+    // println!("day 1: p1 {} | p2 {}", day_01::part_1(), day_01::part_2());
+    println!("day 2: p1 {} | p2 {}", day_02::part_1(), day_02::part_2());
 }
