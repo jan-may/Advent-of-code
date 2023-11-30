68b296fa422afc148428594148de8745c970a67d
Jan May <jan_may@gmx.net>
2023-11-30 12:40:54 +0100

    prepare for 2023

---
+++ y_2023/src/day_01.rs
@@ -0,0 +1,21 @@
+use crate::lib::parse_input;
+
+pub fn part_1() -> i32 {
+    0
+}
+
+pub fn part_2() -> i32 {
+    0
+}
+
+// tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1() {}
+
+    #[test]
+    fn test_part_2() {}
+}
